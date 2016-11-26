118a4e29164f33adf8a30d03d0f20ed5f8f7dcf7
“Daice” <zmzkee@gmail.com>
2016-11-26 22:45:14 +0800

    20,21

---
+++ 1-50/20.Valid-Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+	bool isValid(string s){
+		stack<char> p;
+		for(auto x:s){
+			switch(c){
+				case '(':
+				case '[':
+				case '{':p.push(c);break;
+				case ')':if(p.empty()||p.top()!='(') return false; else p.pop();break;
+
+				case ']':if(p.empty()||p.top()!='[') return false; else p.pop();break;
+				case '}':if(p.empty()||p.top()!='{') return false; else p.pop();break;
+			}
+		}
+		return p.empty();
+	}
+}
